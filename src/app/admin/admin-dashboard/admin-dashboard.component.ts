df2a2d2405144c3b35ba51bcfe0aca9748266374
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-06 17:22:55 +0000

    feat: add ddmin dashboard

---
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.scss'],
+})
+export class AdminDashboardComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
