b5f5b7ef4bee3a498a4b5271371a8d31c94d963a
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-06 20:02:58 +0000

    feat: add form in episode form to handle editing an episode

--- src/app/admin/episodes/episode-form/episode-form.component.ts
+++ src/app/admin/episodes/episode-form/episode-form.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { EpisodeService } from '../../../core/services/episode.service';
+import { Episode } from '../../../model/podcast.models';
 
 @Component({
   selector: 'app-episode-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './episode-form.component.html',
-  styleUrl: './episode-form.component.scss'
+  styleUrl: './episode-form.component.scss',
 })
-export class EpisodeFormComponent {
+export class EpisodeFormComponent implements OnInit {
+  form!: FormGroup;
+  isEdit = false;
+  episodeId: number | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private episodeService: EpisodeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      audio_url: ['', Validators.required],
+    });
+
+    this.episodeId = Number(this.route.snapshot.paramMap.get('id'));
+    this.isEdit = !!this.episodeId;
+
+    if (this.isEdit) {
+      this.episodeService.getEpisodeById(this.episodeId).subscribe((ep) => {
+        this.form.patchValue(ep);
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const episodeData = this.form.value;
+
+    if (this.isEdit) {
+      this.episodeService
+        .updateEpisode(this.episodeId!, episodeData)
+        .subscribe(() => {
+          this.router.navigate(['/admin/episodes']);
+        });
+    } else {
+      this.episodeService.createEpisode(episodeData).subscribe(() => {
+        this.router.navigate(['/admin/episodes']);
+      });
+    }
+  }
 }
