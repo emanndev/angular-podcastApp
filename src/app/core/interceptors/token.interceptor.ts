aca75ed5a81f3c698fad4c25b476febb0cf19ebe
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-06 09:31:00 +0000

    feat: add interceptor logic for http requests

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,5 +1,21 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpInterceptorFn,
+  HttpInterceptor,
+  HttpRequest,
+  HttpEvent,
+} from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(AuthService).getToken();
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloned);
+  }
   return next(req);
 };
