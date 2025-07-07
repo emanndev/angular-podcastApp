bb11247a105c57165f9cabfd6acf3372dfdf5983
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-07 01:46:30 +0000

    feat: add logic to handle playlists

--- src/app/admin.routes.ts
+++ src/app/admin.routes.ts
@@ -3,6 +3,8 @@ import { AdminLayoutComponent } from './pages/admin-layout/admin-layout.componen
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { AdminConfessionsComponent } from './admin/admin-confessions/admin-confessions.component';
 import { AdminEpisodesComponent } from './admin/admin-episodes/admin-episodes.component';
+import { AdminPlaylistsComponent } from './admin/admin-playlists/admin-playlists.component';
+import { CreatePlaylistComponent } from './admin/playlists/create-playlist/create-playlist.component';
 
 export const adminRoutes: Routes = [
   {
@@ -12,6 +14,15 @@ export const adminRoutes: Routes = [
       { path: 'dashboard', component: AdminDashboardComponent },
       { path: 'confessions', component: AdminConfessionsComponent },
       { path: 'episodes', component: AdminEpisodesComponent },
+      { path: 'playlists', component: AdminPlaylistsComponent },
+      {
+        path: 'playlists/create',
+        loadComponent: () =>
+          import(
+            './admin/playlists/create-playlist/create-playlist.component'
+          ).then((m) => m.CreatePlaylistComponent),
+        //   canActivate: [authGuard],
+      },
     ],
   },
 ];
--- src/app/core/services/playlist.service.ts
+++ src/app/core/services/playlist.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Playlist, ApiResponse } from '../../model/podcast.models';
+import {
+  Playlist,
+  PlaylistForm,
+  Episode,
+  ApiResponse,
+} from '../../model/podcast.models';
 import { Observable, map } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
@@ -14,5 +19,33 @@ export class PlaylistService {
     return this.http
       .get<ApiResponse<Playlist[]>>(`${this.baseUrl}/playlists`)
       .pipe(map((res) => res.data));
+  }
+
+  getAllEpisodes(): Observable<Episode[]> {
+    return this.http
+      .get<ApiResponse<Episode[]>>(`${this.baseUrl}/episodes`)
+      .pipe(map((res) => res.data));
+  }
+
+  createPlaylist(data: PlaylistForm): Observable<Playlist> {
+    return this.http
+      .post<ApiResponse<Playlist>>(`${this.baseUrl}/playlists`, data)
+      .pipe(map((res) => res.data));
+  }
+
+  getPlaylistById(id: number): Observable<Playlist> {
+    return this.http
+      .get<ApiResponse<Playlist>>(`${this.baseUrl}/playlists/${id}`)
+      .pipe(map((res) => res.data));
+  }
+
+  updatePlaylist(id: number, data: PlaylistForm): Observable<Playlist> {
+    return this.http
+      .put<ApiResponse<Playlist>>(`${this.baseUrl}/playlists/${id}`, data)
+      .pipe(map((res) => res.data));
+  }
+
+  deletePlaylist(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/playlists/${id}`);
   }
 }
