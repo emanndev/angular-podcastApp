c5d367cebcfa12c4d412d50183a1e07e12d84c41
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-07 14:28:02 +0000

    feat: add toast service to logout method in admin layout

--- src/app/pages/admin-layout/admin-layout.component.ts
+++ src/app/pages/admin-layout/admin-layout.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Router, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { AdminDashboardComponent } from '../../admin/admin-dashboard/admin-dashboard.component';
+import { ToastService } from '../../shared/utils/services/toast.service';
 
 @Component({
   selector: 'app-admin-layout',
@@ -14,7 +15,11 @@ import { AdminDashboardComponent } from '../../admin/admin-dashboard/admin-dashb
 export class AdminLayoutComponent {
   isSidebarOpen = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toast: ToastService
+  ) {}
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
@@ -26,6 +31,7 @@ export class AdminLayoutComponent {
 
   logout() {
     this.authService.logout();
+    this.toast.show('You have successfully logged out', 'success');
     this.router.navigate(['/login']);
   }
 }
