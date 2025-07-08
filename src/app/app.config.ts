ffe9792afa6b1f831a0897a26742bfc335c324f4
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-08 16:51:26 +0000

    feat: install and add Angular material to app.config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
 import { routes } from './app.routes';
+import { MATERIAL_MODULES } from './shared/utils/components/material/material.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    MATERIAL_MODULES,
+    provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
